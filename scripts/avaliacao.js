7b3cda143a1ca8400c95c9e70a0d837cd13eff80
bruna <brunaduarte2307@gmail.com>
2026-03-18 20:07:14 -0300

    teste avaliacao renan

---
+++ scripts/avaliacao.js
@@ -0,0 +1,25 @@
+//function
+//getElementById
+//addEventListener
+
+// questao 1
+
+function exemplo1(){
+    //some dois numeros inteiros
+    const numero = Number(prompt("Digite um numero"))
+    const numero2 = Number(prompt("Digite outro numero"))
+
+    alert(numero+numero2)
+} // termina 
+
+const buttonquestao1 =  document.getElementById("questao1")
+buttonquestao1.addEventListener("click", () => { exemplo1() } )
+
+// questao 2
+
+function exemplo2(){
+   
+    
+}
+const buttonquestao2 =  document.getElementById("questao2")
+buttonquestao2.addEventListener("click", () => { exemplo2() } )
